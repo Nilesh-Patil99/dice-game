966c30e5178a27e157e2bd55c4dcef55af599cb4
Nilesh-Patil99 <137296878+Nilesh-Patil99@users.noreply.github.com>
2023-07-19 13:07:21 +0530

    index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+
 var player1 = Math.floor(Math.random()*6+1)
 var player2 = Math.floor(Math.random()*6+1)
 
 document.querySelector(".img1").setAttribute("src", './images/dice'+player1+'.png')
 document.querySelector(".img2").setAttribute("src", './images/dice'+player2+'.png')
-if(player1 == player2){
-    document.querySelector(".one").innerHTML="try again";}
-
-else if(player1>player2){
-    document.querySelector(".one").innerHTML="Player 1 won";
+if(player1==player2){
+  document.querySelector(".winner").innerHTML="Try Again";
+}else if(player1>player2){
+    document.querySelector(".winner").innerHTML="Player 1 won";
 }else{
-    document.querySelector(".one").innerHTML="Player 2 won";
+    document.querySelector(".winner").innerHTML="Player 2 won";
 }
+    
