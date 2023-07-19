7ac8b087db6b8913aa59b69a9d62e70b53fada97
Nilesh-Patil99 <137296878+Nilesh-Patil99@users.noreply.github.com>
2023-07-19 12:16:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var player1 = Math.floor(Math.random()*6+1)
+var player2 = Math.floor(Math.random()*6+1)
+
+document.querySelector(".img1").setAttribute("src", './images/dice'+player1+'.png')
+document.querySelector(".img2").setAttribute("src", './images/dice'+player2+'.png')
+
+if(player1>player2){
+    document.querySelector(".winner")="Player 1 won";
+}else{
+    document.querySelector(".winner")="Player 2 won";
+}
